8afe2cdf641549c0028ae581be8df9360c5621aa
koumoin <88896694+koumoin@users.noreply.github.com>
2022-01-18 04:12:29 +0100

    Add files via upload

---
+++ exo1/MonAplication1.java
@@ -0,0 +1,15 @@
+public class MonAplication1 {
+
+
+	public static void main (String arg[]) {
+	System.out.println("démo condition");
+	
+	int i = 10; // déclaration+affectation
+	
+	if (i < 0)
+	
+		System.out.println("Le nombre est negatif");
+	else
+		System.out.println("Le nombre est positif");
+		}
+}	
---
+++ exo1/MonAplication2.java
@@ -0,0 +1,15 @@
+public class MonAplication2 {
+
+
+	public static void main (String arg[]) {
+	System.out.println("démo condition");
+	
+	int i = 58; 
+	
+	if (i < 100 && i > 50)
+	
+		System.out.println("Le nombre est bien dans l'intervalle");
+	else
+		System.out.println("Le nombre n'est pas dans l'intervalle");
+		}
+}	
